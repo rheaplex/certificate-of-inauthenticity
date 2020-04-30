eee39ed71e9699f703b10bd5497d3d7fc29bf159
Rhea Myers <rhea@myers.studio>
2020-04-29 20:50:33 -0700

    Add token URI support to the contract and migration. Test this.

--- contracts/CertificateERC721.sol
+++ contracts/CertificateERC721.sol
@@ -1,13 +1,125 @@
 pragma solidity ^0.5.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Metadata.sol";
+import "@openzeppelin/contracts/introspection/ERC165.sol";
+
+
+// We don't use the ERC721Metadata implementation as we don't need the storage
+// and functions it adds.
 
 contract CertificateERC721
-is ERC721Full("Certificate of Inauthenticity", "CINAUTH") {
-  constructor() public {
-      // Mint 3 x 11 tokens
-      for(uint256 i = 1; i <= 33; i++) {
-          _mint(msg.sender, i);
-      }
-  }
+is ERC165, ERC721, ERC721Enumerable, IERC721Metadata
+() {
+    // Token name
+    string private _name;
+
+    // Token symbol
+    string private _symbol;
+
+    // Base URI
+    string private _baseURI;
+
+    /*
+     *     bytes4(keccak256('name()')) == 0x06fdde03
+     *     bytes4(keccak256('symbol()')) == 0x95d89b41
+     *     bytes4(keccak256('tokenURI(uint256)')) == 0xc87b56dd
+     *
+     *     => 0x06fdde03 ^ 0x95d89b41 ^ 0xc87b56dd == 0x5b5e139f
+     */
+    bytes4 private constant _INTERFACE_ID_ERC721_METADATA = 0x5b5e139f;
+
+    /**
+     * @dev Constructor function
+     */
+    constructor (
+        string memory name,
+        string memory symbol,
+        string memory baseURI
+    ) public {
+        _name = name;
+        _symbol = symbol;
+        _baseURI = baseURI;
+
+        // register the supported interfaces to conform to ERC721 via ERC165
+        _registerInterface(_INTERFACE_ID_ERC721_METADATA);
+
+        // Mint 3 x 11 tokens
+        for(uint256 i = 1; i <= 33; i++) {
+            _mint(msg.sender, i);
+        }
+    }
+
+    /**
+     * @dev Gets the token name.
+     * @return string representing the token name
+     */
+    function name() external view returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Gets the token symbol.
+     * @return string representing the token symbol
+     */
+    function symbol() external view returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns the URI for a given token ID. May return an empty string.
+     *
+     * If the token's URI is non-empty and a base URI was set (via
+     * {_setBaseURI}), it will be added to the token ID's URI as a prefix.
+     *
+     * Reverts if the token ID does not exist.
+     */
+    function tokenURI(uint256 tokenId) external view returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        // abi.encodePacked is being used to concatenate strings
+        return string(abi.encodePacked(_baseURI, uint2str(tokenId)));
+    }
+
+    /**
+     * @dev Internal function to set the base URI for all token IDs. It is
+     * automatically added as a prefix to the value returned in {tokenURI}.
+     *
+     * _Available since v2.5.0._
+     */
+    function _setBaseURI(string memory baseURI) internal {
+        _baseURI = baseURI;
+    }
+
+    /**
+    * @dev Returns the base URI set via {_setBaseURI}. This will be
+    * automatically added as a preffix in {tokenURI} to each token's URI, when
+    * they are non-empty.
+    *
+    * _Available since v2.5.0._
+    */
+    function baseURI() external view returns (string memory) {
+        return _baseURI;
+    }
+
+    // via https://github.com/oraclize/ethereum-api/blob/master/oraclizeAPI_0.5.sol
+
+    function uint2str(uint i) internal pure returns (string memory) {
+        if (i == 0) return "0";
+        uint j = i;
+        uint len;
+        while (j != 0){
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+        while (i != 0){
+            bstr[k--] = byte(48 + uint8(i % 10));
+            i /= 10;
+        }
+        return string(bstr);
+    }
 }
--- test/certificate_e_r_c721.js
+++ test/certificate_e_r_c721.js
@@ -1,9 +1,46 @@
-var CertificateERC721 = artifacts.require("CertificateERC721");
+const fs = require('fs');
 
-contract("CertificateERC721", function(accounts) {
-  it("should assert true", function(done) {
-    var certificate_e_r_c721 = CertificateERC721.deployed();
-    assert.isTrue(true);
-    done();
+
+const CertificateERC721 = artifacts.require("CertificateERC721");
+
+const dirHash= fs.readFileSync('ipfs-metadata-dir-hash').toString('utf-8');
+assert(dirHash != '');
+
+const NUM_TOKENS = 33;
+const TOKEN_NAME = "Certificate of Inauthenticity";
+const TOKEN_SYMBOL = "COIN";
+// Must have trailing slash
+const baseURI = `/ipfs/${dirHash}/`;
+
+
+contract("CertificateERC721", accounts => {
+  let token;
+
+  before(async () => {
+    token = await CertificateERC721.deployed();
   });
+
+  it("should be configured correctly on deployment", async () => {
+    assert.equal(await token.name(), TOKEN_NAME);
+    assert.equal(await token.symbol(), TOKEN_SYMBOL);
+    assert.equal(await token.baseURI(), baseURI);
+    assert.equal(await token.balanceOf(accounts[0]), NUM_TOKENS);
+  });
+
+  it("should return correct token URIs", async () => {
+    for (let i = 1; i < NUM_TOKENS + 1; i++) {
+      assert.equal(
+        await token.tokenURI(i),
+        `${baseURI}${i}`
+      );
+    }
+  });
+
+  it("should transfer tokens from owner", async () => {
+    await token.transferFrom(accounts[0], accounts[1], 1);
+    assert.equal(await token.ownerOf(1), accounts[1]);
+    assert.equal(await token.balanceOf(accounts[1]), 1);
+  });
+
+  // And if it does that it should be OK. :-)
 });
