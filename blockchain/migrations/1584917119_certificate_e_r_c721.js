4dc47de82aa7907d998d0f6534c2d0c683a423ee
Rob Myers <rob@robmyers.org>
2020-04-29 20:50:33 -0700

    Add token URI support to the contract and migration. Test this.

--- blockchain/migrations/1584917119_certificate_e_r_c721.js
+++ blockchain/migrations/1584917119_certificate_e_r_c721.js
@@ -1,5 +1,20 @@
+const assert = require('assert');
+const fs = require('fs');
+
+
 const CertificateERC721 = artifacts.require("CertificateERC721");
 
+
+const dirHash= fs.readFileSync('../ipfs-metadata-dir-hash').toString('utf-8');
+assert(dirHash != '');
+
+
+const TOKEN_NAME = "Certificate of Inauthenticity";
+const TOKEN_SYMBOL = "COIN";
+// Must have trailing slash
+const baseURI = `/ipfs/${dirHash}/`;
+
+
 module.exports = function(deployer) {
-  deployer.deploy(CertificateERC721);
+  deployer.deploy(CertificateERC721, TOKEN_NAME, TOKEN_SYMBOL, baseURI);
 };
