5017b460fde3c92c69f4641419f0809447a8ab56
Rob Myers <rob@robmyers.org>
2020-05-10 16:47:54 -0700

    Get the Truffle configuration ready for deployment.

--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -1,40 +1,6 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
@@ -47,6 +13,23 @@ module.exports = {
     //  port: 8545,            // Standard Ethereum port (default: none)
     //  network_id: "*",       // Any network (default: none)
     // },
+
+    mainnet: {
+      //provider: () => new HDWalletProvider(process.env.MNEMONIC,
+      //                                     "http://127.0.0.1:8545",
+      //                                     process.env.ADDRESS_INDEX || 1),
+      provider: () => new HDWalletProvider(process.env.MNEMONIC,
+                                           `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+                                           process.env.ADDRESS_INDEX || 1),
+      //host: "127.0.0.1",
+      //port: 8545,
+      network_id: "*",
+      gas: 6700000,
+      gasPrice: 7000000000,  // 7 gwei (in wei) (default: 100 gwei)
+      // websockets: true,   // Enable EventEmitter interface for web3 (default: false)
+      timeoutBlocks: 200,    // # of blocks before a deployment times out  (minimum/default: 50),
+      confirmations: 1
+    },
 
     // Another network with more advanced options...
     // advanced: {
