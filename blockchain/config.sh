899b4a7067be85633fbc80ecde62e179dcf872d6
Rhea Myers <rhea@hey.com>
2022-05-03 17:29:41 -0700

    Move metadata to ipfs.

--- blockchain/config.sh
+++ blockchain/config.sh
@@ -3,5 +3,6 @@ EDITION_EACH=11
 
 BUILD_DIR="."
 IMAGE_DIR="${BUILD_DIR}/certificates"
+METADATA_DIR="${BUILD_DIR}/ipfs-metadata"
 IMAGE_DIR_HASH_FILE="ipfs-certificate-dir-hash"
 METADATA_DIR_HASH_FILE="ipfs-metadata-dir-hash"
